dc0e4aeeb69d884a4262e0d2c5622abcf126781c
TomatoRadio <leanary.11@gmail.com>
2025-09-28 00:54:24 -0400

    Compatibility Fix for TR_OrderedSkills

--- Battle System Plugins/TR_OrderedSkills.js
+++ Battle System Plugins/TR_OrderedSkills.js
@@ -8,12 +8,12 @@ Imported.TR_OrderedSkills = true;
 
 var TR = TR || {};
 TR.OS = TR.OS || {};
-TR.OS.version = 1.1;
+TR.OS.version = 1.2;
 
 /*: 
  *
  * @plugindesc Allows hardcoded ordering of the skills in an actor's skill select menu.
- * Version 1.1
+ * Version 1.2
  * @author TomatoRadio
  * 
  * @help
@@ -36,25 +36,34 @@ Examples:
 
 // DO NOT TOUCH THIS
 
-Window_OmoMenuActorSkillEquip.prototype.skillAtIndex = function(index) {
-  // Get Index
-  if (index === undefined) { index = this.index(); };  
-  // Get Actor At Index
-  var actor = this.actor();
-  // If Actor Exists
-  if (actor) {
-    // Return Actor Equips slotted at index
-    return actor.orderedSkills()[index];
+Window_OmoMenuActorSkillList.prototype.makeItemList = function() {
+  if (this._actor) {
+    this._data = this._actor.equipableSkills().filter(function(item) {
+      return this.includes(item);
+    }, this);
+    var temp = [];
+    for (const index of this._actor.orderedSkills()) {
+      this._data.forEach(function(skill) {
+        if (skill.id === index) temp.push(skill);
+      })
+    }
+    var temp2 = this._actor.equipableSkills().filter((skill) => !temp.includes(skill), this)
+    for (const skill of temp2) {
+      temp.push(skill);
+    }
+    this._data = temp
+  } else {
+    this._data = [];
   };
-  // Return null
-  return null;
+  // Push Unequip Null Data
+  this._data.push(null);
 };
 
 Game_Actor.prototype.orderedSkills = function() {
   var skills = this.skills()
   var returnArray = [];
-  if (TR.OS.OrderedSkills[this._actorId].orderedSkills && Array.isArray(TR.OS.OrderedSkills[this._actorId])) {
-      for (const skill of TR.OS.OrderedSkills[this._actorId]) {
+  if ($dataActors[this._actorId].orderedSkills && Array.isArray($dataActors[this._actorId].orderedSkills)) {
+      for (const skill of $dataActors[this._actorId].orderedSkills) {
         if (skills.includes(skill)) returnArray.push(skill);
       }
   }
@@ -64,5 +73,4 @@ Game_Actor.prototype.orderedSkills = function() {
     }
   }
   return returnArray;
-
 }
