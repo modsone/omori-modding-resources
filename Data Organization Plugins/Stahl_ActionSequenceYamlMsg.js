199d048dca0db7a91df45ba7a433a4fae3076a93
StahlReyn <reynstahl@gmail.com>
2025-11-19 23:18:17 -0500

    Update Stahl_ActionSequenceYamlMsg.js

--- Data Organization Plugins/Stahl_ActionSequenceYamlMsg.js
+++ Data Organization Plugins/Stahl_ActionSequenceYamlMsg.js
@@ -1,5 +1,5 @@
  /*:
- * @plugindesc v1.0.0 Makes Battle Text addable with YAML
+ * @plugindesc v1.1.0 Makes Battle Text addable with YAML
  * @author ReynStahl
  * 
  * @help
@@ -34,6 +34,12 @@
  *   targetEnemyText: "All foes' BUFFS were removed!"
  * 
  * ================================================================================
+ * 
+ * 1.1.0:
+ * - Added addFinishText(msgName, bubble = false) which adds message at the end
+ *   of action, similar to how plot armor shows message
+ * - Added addForcedText(msgName, bubble = false) which adds message immediately
+ *   Basically same as showLanguageMessage, just with bubble switching option for battles
  */
 
 var Stahl = Stahl || {};
@@ -89,6 +95,46 @@ BattleManager.yamlText = function(msgName) {
   }
   return true;
 };
+
+BattleManager.yamlTextFormat = function(msgName, s1, s2) {
+  try {
+    var data = LanguageManager.getTextData(this.getYamlFileName(), msgName);
+    var text = this.getTextFromYamlData(data);
+    text = text.format(s1, s2); // Formats - %1 is Subject, %2 is Target
+    this.addTextSplit(text);
+  } catch (e) {
+    console.log('YAMLMSG MESSAGE NOT FOUND');
+    return false;
+  }
+  return true;
+};
+
+/**
+ * Adds text at the end of action. Similar to Plot Armor message
+ * @param {*} msgName 
+ * @param {*} bubble 
+ */
+BattleManager.addFinishText = function(msgName, bubble = false) {
+  let originalBubble = $gameSwitches.value(6);
+  $gameTemp._addToFinishActions = [
+    ["EVAL", [`$gameSwitches.setValue(6, ${bubble.toString()})`]],
+    ["EVAL", [`$gameMessage.showLanguageMessage("${msgName}")`]],
+    ["EVAL", [`$gameSwitches.setValue(6, ${originalBubble.toString()})`]]
+  ]
+}
+
+/**
+ * Adds text immediately. 
+ * Basically same as showLanguageMessage, just with bubble switching option for battles
+ * @param {*} msgName 
+ * @param {*} bubble 
+ */
+BattleManager.addForcedText = function(msgName, bubble = false) {
+  let originalBubble = $gameSwitches.value(6);
+  $gameSwitches.setValue(6, bubble)
+  $gameMessage.showLanguageMessage(msgName)
+  $gameSwitches.setValue(6, originalBubble)
+}
 
 // Adds own function if it does not already exist.
 if (BattleManager.addTextsplit == undefined) {
