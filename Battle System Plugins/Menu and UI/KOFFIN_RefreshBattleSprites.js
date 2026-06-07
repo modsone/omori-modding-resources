622bb511f1e6c8913492cd9c97ea84e1ba9e9206
KoffinKrypt <114689865+Nebulaphile@users.noreply.github.com>
2026-06-07 00:23:45 -0300

    HP/MP no longer reanimates, Z-index is preserved

--- Battle System Plugins/Menu and UI/KOFFIN_RefreshBattleSprites.js
+++ Battle System Plugins/Menu and UI/KOFFIN_RefreshBattleSprites.js
@@ -1,89 +1,180 @@
 /*:
  * @plugindesc Refresh Party Members In-battle
- * @author KoffinKrypt
+ * @author KoffinKrypt 
  *
  * @help
- * Makes so that you can refresh the party members' battle portraits
- * after adding them to the party mid-battle so that the portraits
- * of the new party member are loaded.
+ * Automatically refreshes party members' battle portraits after adding
+ * or removing party members mid-battle.
  *
- * Note: This also works with removing party members, and it will
- * delete the party member's battle portrait after they're removed!
+ * V1.3 - Preserves z-index and prevents HEART/JUICE bars from re-animating on refresh.
  *
- * How to use:
- * Use the script call command in a battle event page after either
- * adding or removing a party member and type this:
- * SceneManager._scene.refreshFaceWindows();
- * 
- * 
  */
 
-Scene_Battle.prototype.refreshFaceWindows = function() {
-  // Remove existing face windows
-  this.removeFaceWindows();
+(function() {
+    // Store original Game_Party methods
+    var _Game_Party_addActor = Game_Party.prototype.addActor;
+    var _Game_Party_removeActor = Game_Party.prototype.removeActor;
 
-  // Create Face Windows Container
-  this._faceWindowsContainer = new Sprite();
-  this.addChild(this._faceWindowsContainer);
+    // Override addActor to refresh battle sprites when in battle
+    Game_Party.prototype.addActor = function(actorId) {
+        var wasInBattle = $gameParty.inBattle();
+        var result = _Game_Party_addActor.call(this, actorId);
+        
+        if (wasInBattle && SceneManager._scene instanceof Scene_Battle) {
+            SceneManager._scene.refreshFaceWindows(true); // Pass true to indicate refresh from party change
+        }
+        
+        return result;
+    };
 
-  // Initialize
-  this._faceWindowsContainer._displayLayersContainer = new Sprite();
-  this._faceWindowsContainer.addChild(this._faceWindowsContainer._displayLayersContainer);
+    // Override removeActor to refresh battle sprites when in battle
+    Game_Party.prototype.removeActor = function(actorId) {
+        var wasInBattle = $gameParty.inBattle();
+        var result = _Game_Party_removeActor.call(this, actorId);
+        
+        if (wasInBattle && SceneManager._scene instanceof Scene_Battle) {
+            SceneManager._scene.refreshFaceWindows(true); // Pass true to indicate refresh from party change
+        }
+        
+        return result;
+    };
 
-  // Get Layers container
-  var layers = this._faceWindowsContainer._displayLayersContainer;
-
-  // Create Layer List
-  var layerList = ['behind', 'statusBack', 'face', 'polaroid', 'front'];
-
-  // Create Layers
-  for (var i = 0; i < layerList.length; i++) {
-    var name = '_' + layerList[i];
-    var layer = new Sprite();
-    layers[name] = layer;
-    layers.addChild(layer);
-  }
-
-  // Initialize Face Windows Array
-  this._faceWindows = [];
-
-  // Position Indexes
-  var indexes = [2, 3, 0, 1];
-
-  // Create Face Windows
-  for (var i = 0; i < 4; i++) {
-    // Get Index
-    var index = i;
-    var x = 14 + ((index % 2) * ((Graphics.width - 28) - 114));
-    var y = 5 + (Math.floor(index / 2) * ((Graphics.height - 16) - 164));
-
-    // Create Face Window
-    var faceWindow = new Window_OmoriBattleActorStatus(i, layers, x, y);
-    faceWindow.x = !faceWindow.actor() ? i % 2 ? Graphics.width : -faceWindow.windowWidth() : x;
-    faceWindow.y = y;
-    this._faceWindows[i] = faceWindow;
-    this._faceWindowsContainer.addChild(faceWindow);
-  }
-
-  // Create Animation Sprite for Layers
-  for (var i = 0; i < layerList.length; i++) {
-    // Get Layer
-    layer = layers['_' + layerList[i]];
-
-    // Create Face Animation Sprite
-    var sprite = new Sprite_BattleFaceAnimation();
-    sprite._effectTarget = sprite;
-    layers['_animation' + i] = sprite;
-    layer.addChild(sprite);
-  }
-
-  // Set status Windows Face Windows
-  this._statusWindow._faceWindows = this._faceWindows;
-};
-
-Scene_Battle.prototype.removeFaceWindows = function() {
-  // Remove existing face windows
-  if (this._faceWindowsContainer) {
-    this._faceWindowsContainer.parent.removeChild(this._faceWindowsContainer);
-  }
-};
+    // Store original refresh method
+    var _Scene_Battle_refreshFaceWindows = Scene_Battle.prototype.refreshFaceWindows;
+    
+    // Override refreshFaceWindows to preserve picture layers and prevent HP/MP animation
+    Scene_Battle.prototype.refreshFaceWindows = function(skipAnimation) {
+        // Store the picture container and its current Z-index
+        var pictureContainer = this._spriteset ? this._spriteset._pictureContainer : null;
+        var pictureIndex = -1;
+        var pictureParent = null;
+        
+        if (pictureContainer && this._spriteset) {
+            pictureParent = this._spriteset;
+            pictureIndex = pictureParent.children.indexOf(pictureContainer);
+        }
+        
+        // Store other important elements that might be affected
+        var stressBar = this._stressBar;
+        var stressBarParent = stressBar ? stressBar.parent : null;
+        var stressBarIndex = -1;
+        if (stressBarParent && stressBar) {
+            stressBarIndex = stressBarParent.children.indexOf(stressBar);
+        }
+        
+        // Store low HP overlay
+        var lowHpOverlay = this._lowHpOverlay;
+        var lowHpOverlayParent = lowHpOverlay ? lowHpOverlay.parent : null;
+        var lowHpOverlayIndex = -1;
+        if (lowHpOverlayParent && lowHpOverlay) {
+            lowHpOverlayIndex = lowHpOverlayParent.children.indexOf(lowHpOverlay);
+        }
+        
+        // Store current HP/MP values for each actor before refresh
+        var currentHpValues = [];
+        var currentMpValues = [];
+        if (skipAnimation && this._faceWindows) {
+            for (var i = 0; i < this._faceWindows.length; i++) {
+                var win = this._faceWindows[i];
+                var actor = win.actor();
+                if (actor) {
+                    currentHpValues[i] = actor.hp;
+                    currentMpValues[i] = actor.mp;
+                }
+            }
+        }
+        
+        // Call original refresh (removes and recreates face windows)
+        if (_Scene_Battle_refreshFaceWindows) {
+            _Scene_Battle_refreshFaceWindows.call(this);
+        } else {
+            // Original logic if no stored method
+            if (this._faceWindowsContainer) {
+                this.removeChild(this._faceWindowsContainer);
+                this._faceWindowsContainer = null;
+            }
+            this.createFaceWindows();
+            this.createActorWindow();
+        }
+        
+        // If skipping animation, set animation durations to 0 and force correct values
+        if (skipAnimation && this._faceWindows) {
+            for (var i = 0; i < this._faceWindows.length; i++) {
+                var win = this._faceWindows[i];
+                var actor = win.actor();
+                if (actor) {
+                    // Set animation durations to 0 to prevent animation
+                    if (win._hpAnim) {
+                        win._hpAnim.duration = 0;
+                        win._hpAnim.current = actor.hp;
+                        win._hpAnim.target = actor.hp;
+                        win._hpAnim.old = actor.hp;
+                    }
+                    if (win._mpAnim) {
+                        win._mpAnim.duration = 0;
+                        win._mpAnim.current = actor.mp;
+                        win._mpAnim.target = actor.mp;
+                        win._mpAnim.old = actor.mp;
+                    }
+                    
+                    // Force redraw of HP and MP values
+                    win.drawHP(actor.hp, actor.mhp);
+                    win.drawMP(actor.mp, actor.mmp);
+                    
+                    // Force update bar widths
+                    if (win._hpBarSprite) {
+                        var hpWidth = (actor.hp / actor.mhp) * 81;
+                        win._hpBarSprite._frame.width = hpWidth;
+                        win._hpBarSprite._refresh();
+                    }
+                    if (win._mpBarSprite) {
+                        var mpWidth = (actor.mp / actor.mmp) * 81;
+                        win._mpBarSprite._frame.width = mpWidth;
+                        win._mpBarSprite._refresh();
+                    }
+                }
+            }
+        }
+        
+        // Restore picture container to its original Z-index
+        if (pictureContainer && pictureParent && pictureIndex !== -1) {
+            // Check if it's still in the parent
+            var currentIndex = pictureParent.children.indexOf(pictureContainer);
+            if (currentIndex !== pictureIndex && currentIndex !== -1) {
+                pictureParent.removeChild(pictureContainer);
+                pictureParent.addChildAt(pictureContainer, pictureIndex);
+            } else if (currentIndex === -1) {
+                // Reattach if it got removed
+                pictureParent.addChildAt(pictureContainer, Math.min(pictureIndex, pictureParent.children.length));
+            }
+        }
+        
+        // Restore stress bar position
+        if (stressBar && stressBarParent && stressBarIndex !== -1) {
+            var currentStressIndex = stressBarParent.children.indexOf(stressBar);
+            if (currentStressIndex !== stressBarIndex && currentStressIndex !== -1) {
+                stressBarParent.removeChild(stressBar);
+                stressBarParent.addChildAt(stressBar, stressBarIndex);
+            }
+        }
+        
+        // Restore low HP overlay position
+        if (lowHpOverlay && lowHpOverlayParent && lowHpOverlayIndex !== -1) {
+            var currentOverlayIndex = lowHpOverlayParent.children.indexOf(lowHpOverlay);
+            if (currentOverlayIndex !== lowHpOverlayIndex && currentOverlayIndex !== -1) {
+                lowHpOverlayParent.removeChild(lowHpOverlay);
+                lowHpOverlayParent.addChildAt(lowHpOverlay, lowHpOverlayIndex);
+            }
+        }
+        
+        // Force refresh stress bar bitmap
+        if (this._stressBar && this._stressBar.refreshEKGBitmap) {
+            this._stressBar.refreshEKGBitmap();
+        }
+        
+        // Reapply picture display layer setting
+        if (BattleManager._pictureDisplayLayer) {
+            BattleManager.setPictureDisplayLayer(BattleManager._pictureDisplayLayer);
+        }
+    };
+})();
